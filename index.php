104ebab723f36e21985054268322881fa775d844
BreckoAlexander <72256862+BreckoAlexander@users.noreply.github.com>
2022-09-19 20:25:17 +0200

    php variable

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    echo "Hello Azure!";
+    echo "<h2>Hello Azure!</h2>";
 
-    $input_1 = $_GET["a"]
-    $input_2 = $_GET["b"]
+    $input_1 = $_GET["a"];
+    $input_2 = $_GET["b"];
 
-    $sum = $input_1 + $input_2
-    echo $sum
+    $sum = $input_1 + $input_2;
+    echo $sum;
 ?>
