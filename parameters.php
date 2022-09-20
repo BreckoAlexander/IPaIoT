73e44876fd7e9d456981b7be78ffe45df9cb7fac
BreckoAlexander <72256862+BreckoAlexander@users.noreply.github.com>
2022-09-20 20:28:03 +0200

    Update parameters.php

--- parameters.php
+++ parameters.php
@@ -1,11 +1,11 @@
 <?php
 
-echo "<h2>Parameters page </h2>
+echo "<h2>Parameters page </h2>";
 
 $input_1 = $_GET["a"];
 $input_2 = $_GET["b"];
 
-$sum = $input_1 + $input_2
+$sum = $input_1 + $input_2;
 
 echo $sum;
 
