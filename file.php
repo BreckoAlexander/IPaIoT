35a5734cc12f9f9bffb591b23b1f89e9244e2a31
Alexander Brecko <brecko@MacBook-Air-5.local>
2022-10-29 19:48:08 +0200

    Update

--- file.php
+++ file.php
@@ -3,7 +3,7 @@
 $sn1 = $_GET["a"];
 $sn2 = $_GET["b"];
 
-$file1 = fopen("sensors.txt","w") or die("Unable to open file!");;
+$file1 = fopen("sensors.txt","w") or die("Unable to open file!");
 $text1 = "a=" . $sn1 . " b=" . $sn2;
 
 fwrite($file1, $text1);
