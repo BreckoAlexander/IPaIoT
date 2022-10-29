54c592a947baca3af8ab976e3fd521f5126d5ce4
Alexander Brecko <brecko@MacBook-Air-5.local>
2022-10-29 19:21:50 +0200

    Add getParameters.php

--- getParameters.php
+++ getParameters.php
@@ -4,8 +4,10 @@ echo '<h1>getParameters page</h1>';
 $sn1 = $_GET["a"];
 $sn2 = $_GET["b"];
 
-$text1 = "a=" . $sn1 . " b=" . $sn2;
+$text = "a=" . $sn1 . " b=" . $sn2;
+$sum = $sn1 + $sn2
 
-echo $text1;
+echo $text;
+echo $sum;
 
 ?>
