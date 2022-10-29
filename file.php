1cb281415683f09334789370b62f7d20a18a0953
Alexander Brecko <brecko@MacBook-Air-5.local>
2022-10-29 19:47:35 +0200

    Update

--- file.php
+++ file.php
@@ -9,9 +9,14 @@ $text1 = "a=" . $sn1 . " b=" . $sn2;
 fwrite($file1, $text1);
 fclose($file1);
 
-$file2 = fopen("actuator.txt","r");
+
+$file2 = fopen("actuator.txt","w");
+fwrite($file2, "Hodnoty z akcneho clena");
+fclose($file2);
+
+$file3 = fopen("actuator.txt","r");
 $text2 = fread($file2,filesize("actuator.txt"));
 echo $text2;
-fclose($file2);
+fclose($file3);
 
 ?>
